7b7accf2f17ac7b6033bfbd34ade88cbc7a00f70
Coder481 <68111551+Coder481@users.noreply.github.com>
2020-09-29 20:00:42 +0530

    Update changeToUpperCase.py

--- String/changeToUpperCase.py
+++ String/changeToUpperCase.py
@@ -2,8 +2,10 @@ def uppercase(str_data):
     '''Python program to convert the string to uppercase without inbuild functions'''
     result = ''
     for char in str_data:
-        if ord(char) >= 65:
+        if ord(char) >= 97 and ord(char)<=122:
             result += chr(ord(char) - 32)
+        else:
+            result+=char
     return result
 
-uppercase("HAI")
+uppercase("Hello World")
