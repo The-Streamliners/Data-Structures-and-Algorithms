bd2316be2c6c6629205d5c7ebd0ec0b47faedcfd
Sameera <aryasamee@gmail.com>
2020-10-01 22:23:02 +0530

    Modified the bubble sort algorithm

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -2,29 +2,36 @@
 using namespace std;
 void BUBBLE_SORT(int A[100], int n)
 {
-    int Start=0;
-    int End=n-1;
-    while(End!=Start)
+    int i,j;
+    bool swap;
+    for(int i=0;i<n-1;i++)
     {
-        int i=Start;
-        while(i<End)
+        swap=false;
+        for(int j=0;j<n-i-1;j++)
         {
-            if(A[i+1]<A[i])
+            if(A[j]>A[j+1])
             {
-                int temp=A[i];
-                A[i]=A[i+1];
-                A[i+1]=temp;
+                int t=A[j];
+                A[j]=A[j+1];
+                A[j+1]=t;
+                swap=true;
             }
-            i=i+1;
         }
-        End=End-1;
+        if(swap==false)
+        {
+            break;
+        }
     }
     return;
+
+
 }
+
 int main()
 {
     int A[]={4,5,6,1,3,8,2,7};
     int n=sizeof(A)/sizeof(A[0]);
+    cout<<n<<endl;
     BUBBLE_SORT(A,n);
     for(int i=0;i<n;i++) cout<<A[i]<<" ";
     cout<<"\n";
