91549763963a0bfa320463f01bd9b8ba335dacf3
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2020-10-02 14:36:47 +0530

    Update and rename vectors.cpp to Vectors-Implementation.cpp

--- Non-Alogrithmic/Vectors-Implementation.cpp
+++ Non-Alogrithmic/Vectors-Implementation.cpp
@@ -1,3 +1,4 @@
+// Complete Implementation of Vectors.
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -6,7 +7,7 @@ int main()
   
   /*
   
-  begin() – Returns an iterator pointing to the first element in the vector
+begin() – Returns an iterator pointing to the first element in the vector
 end() – Returns an iterator pointing to the theoretical element that follows the last element in the vector
 rbegin() – Returns a reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
 rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
