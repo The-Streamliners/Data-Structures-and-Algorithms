acc424d91ac8efe328c893f655d3102f82689e56
surajkrmodi <modisuraj61@gmail.com>
2020-10-05 13:37:54 +0530

    updated recursive function of 0-1 knapsack to memoization

--- Dynamic-Programming/KnapSack01.cpp
+++ Dynamic-Programming/KnapSack01.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
-
+int t[1001][1001];
 int knapSackrecur(int w, int wt[],int val[],int n){
     if(n==0||w==0)
     return 0;
+    if(t[n][w]!=-1)
+    return t[n][w];
     if(wt[n-1]>w)
-    return knapSackrecur(w,wt,val,n-1);
+    {
+		
+		int x= knapSackrecur(w,wt,val,n-1);
+		t[n][w]=x;
+		return t[n][w];
+	}
     else
-    return max(val[n-1]+knapSackrecur(w-wt[n-1],wt,val,n-1),knapSackrecur(w,wt,val,n-1));   
+    {
+		int x= max(val[n-1]+knapSackrecur(w-wt[n-1],wt,val,n-1),knapSackrecur(w,wt,val,n-1));  
+		t[n][w]=x;
+		return t[n][w];
+	} 
 }
 
 int main(){
@@ -22,6 +34,7 @@ int main(){
     cout<<"Enter Values"<<endl;
     for(int i =0;i<n;i++)
     cin>>val[i];
+    memset(t,-1,sizeof(t));
     cout<<knapSackrecur(w,wt,val,n);
 
 }
