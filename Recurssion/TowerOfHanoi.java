bbc66016243483a0ec7100caf8134361494359a5
ayushi_keshri <ayushi.keshri@nearex.com>
2020-10-07 13:25:38 +0530

    build failing issue fixes

--- Recurssion/TowerOfHanoi.java
+++ Recurssion/TowerOfHanoi.java
@@ -1,5 +1,5 @@
 public class TowerOfHanoi{
-     public static void TOH(int n, int A, int B, int C){
+     private static void TOH(int n, int A, int B, int C){
          if(n>0){
              TOH(n-1,A,C,B);
              System.out.println("("+A+","+C+")");
