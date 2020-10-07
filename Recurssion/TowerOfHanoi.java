f26df6be304d6ea178845da21a004574beab1f48
ayushi_keshri <ayushi.keshri@nearex.com>
2020-10-07 10:02:37 +0530

    tower of hanoi in java

---
+++ Recurssion/TowerOfHanoi.java
@@ -0,0 +1,15 @@
+public class TowerOfHanoi{
+     public static void TOH(int n, int A, int B, int C){
+         if(n>0){
+             TOH(n-1,A,C,B);
+             System.out.println("("+A+","+C+")");
+              TOH(n-1,B,A,C);
+         }
+     }
+
+    public static void main(String args[]){
+         TOH(3,1,2,3);
+    }
+    /**Here, A=1,B=2,C=3 are the position of tower */
+   
+}
