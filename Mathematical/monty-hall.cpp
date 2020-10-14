4483fceeb0cb9f7d6a208fee5534a6f1c15da83c
flyinspaghetti <rishabhdas2020@gmail.com>
2020-10-14 19:19:16 +0530

    Added comments in the monty hall program

--- Mathematical/monty-hall.cpp
+++ Mathematical/monty-hall.cpp
@@ -17,24 +17,28 @@ int main()
   chosenDoor = rand() % 3 + 1;
   winningDoor = rand() % 3 + 1;
 
+  /*Beginning of the iterting loop of our simulation*/
   do
   {
+    // If randomly, the revealed door turns out to be the chosen door and the door with the prize inside, this is the condition
     do
     {
       revealedDoor = rand() % 3 + 1;
     } while (revealedDoor == chosenDoor || revealedDoor == winningDoor);
 
+    // If randomly, the remaining door turns out to be the chosen door, we select the remaining door as the revealed door
     do
     {
       remainingDoor = rand() % 3 + 1;
     } while (remainingDoor == chosenDoor || remainingDoor == revealedDoor);
 
+    // IMPORTANT STEP: Written below is the condition for which incrementation happens as per the condition
     option = rand() % 2 + 1;
     if (option == 1)
     {
       if (chosenDoor == winningDoor)
       {
-        stayWins++;
+        stayWins++; //Incrementing the stayWins variable when one simulation wins the prize with the previously chosen door
       }
     }
     if (option == 2)
@@ -42,10 +46,10 @@ int main()
       chosenDoor = remainingDoor;
       if (chosenDoor == winningDoor)
       {
-        switchWins++;
+        switchWins++; //Incrementing the switchWins variable when one simulation wins the prize after switching from the previous choice
       }
     }
-    games++;
+    games++; //Incrementing the total number of games played
   } while (games < 10000);
 
   cout << "Out of all the games, the contestant won " << stayWins << " times by staying with their original choice and won " << switchWins << " times by switching their choice.\n";
