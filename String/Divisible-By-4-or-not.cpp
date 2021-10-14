3752bc8100bd532e6fb1ed904b1b1cfc59736255
Akhil870 <72069148+Akhilesh0947@users.noreply.github.com>
2021-10-14 11:07:19 +0530

    Update and rename divisible by 4 or not.cpp to Divisible-By-4-or-not.cpp

--- String/Divisible-By-4-or-not.cpp
+++ String/Divisible-By-4-or-not.cpp
@@ -11,11 +11,11 @@ int main() {
   int a;
 
   if(s.length()>=2){
-    a=(s[s.length()-2]-'0')*10+(s[s.length()-1]-'0'); // last two digits
+    a=(s[s.length()-2]-'0')*10+(s[s.length()-1]-'0');
   }else{
     a=s[0]-'0'; // single integer
   }
-
+  
   if(a%4==0){
     cout<<"YES"<<endl;
   }else{
